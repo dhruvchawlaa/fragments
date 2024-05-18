b52eb0be93aae64dc5804fab41b38af118e791bd
dhruvchawlaa <dhruvv.chawlaa@gmail.com>
2024-05-18 15:53:54 -0400

    Add dotenv

--- src/server.js
+++ src/server.js
@@ -6,15 +6,22 @@ const stoppable = require('stoppable');
 // Get our logger instance
 const logger = require('./logger');
 
-// Get our express app instance
-const app = require('./app');
-
 // Get the desired port from the process' environment. Default to `8080`
 const port = parseInt(process.env.PORT || '8080', 10);
+
+// Get our express app instance
+const app = require('./app');
 
 // Start a server listening on this port
 const server = stoppable(
   app.listen(port, () => {
+    // Conditional check for printing environment variables
+    if (process.env.LOG_LEVEL === 'debug') {
+      // Log all environment variables
+      logger.debug('Environment variables:');
+      logger.debug(process.env);
+    }
+
     // Log a message that the server has started, and which port it's using.
     logger.info(`Server started on port ${port}`);
   })
