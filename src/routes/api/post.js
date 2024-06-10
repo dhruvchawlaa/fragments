8f57cc013f612e3de3961ed2abd95d2f61e1e4f3
dhruvchawlaa <dhruvv.chawlaa@gmail.com>
2024-06-10 03:13:26 -0400

    Added logging

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -4,22 +4,33 @@ const { Fragment } = require('../../model/fragment');
 const logger = require('../../logger');
 
 module.exports = async (req, res) => {
+  logger.debug('Received request to POST /v1/fragments');
   try {
     if (Buffer.isBuffer(req.body)) {
       logger.info('v1/fragments POST route works');
       const headers = req.headers;
       const contentType = headers['content-type'];
+      logger.debug({ contentType }, 'Received Content-Type');
+
       let fragmentData = new Fragment({
         ownerId: req.user,
         type: contentType,
         size: req.body.length,
       });
       logger.debug({ fragmentData }, 'A fragment is created');
+
       const host = process.env.API_URL || req.headers.host;
       res.location(host + `/v1/fragments/${fragmentData.id}`);
+      logger.info(`Location header set to ${host}/v1/fragments/${fragmentData.id}`);
+
       await fragmentData.save();
+      logger.info('Fragment data saved');
+
       await fragmentData.setData(req.body);
+      logger.info('Fragment data set');
+
       res.status(201).json(createSuccessResponse({ fragment: fragmentData }));
+      logger.info('Response sent with status 201');
     } else {
       logger.warn('Content-Type is not supported for POST');
       res
@@ -30,9 +41,10 @@ module.exports = async (req, res) => {
             'The Content-Type of the fragment being sent with the request is not supported'
           )
         );
+      logger.warn('Response sent with status 415');
     }
   } catch (error) {
-    logger.error(error);
+    logger.error({ error }, 'Internal Server Error');
     res.status(500).json(createErrorResponse(500, 'Internal Server Error'));
   }
 };
