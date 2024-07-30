0946efb581c741f9d044cbee525af70d0f8c4c56
dhruvchawlaa <dhruvv.chawlaa@gmail.com>
2024-07-29 22:02:53 -0400

    Edited cognito env varibles in cd

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,11 +1,13 @@
 // src/auth/index.js
-
+const logger = require('../logger');
 // Prefer Amazon Cognito
 if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
+  logger.info('Using Cognito Authentication');
   module.exports = require('./cognito');
 }
 // Also allow for an .htpasswd file to be used, but not in production
 else if (process.env.HTPASSWD_FILE && process.NODE_ENV !== 'production') {
+  logger.info('Using Basic Authentication');
   module.exports = require('./basic-auth');
 }
 // In all other cases, we need to stop now and fix our config
